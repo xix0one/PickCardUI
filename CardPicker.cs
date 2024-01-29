4f1d629a09893c00e877ef1a0a06988a63afae31
xix0one <petrovalex2014@ya.ru>
2024-01-29 19:07:39 +0800

    Добавьте файлы проекта.

---
+++ CardPicker.cs
@@ -0,0 +1,49 @@
+﻿namespace PickACardUI
+{
+    class CardPicker
+    {
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            int v = random.Next(1, 5);
+
+            switch (v)
+            {
+                case 1:
+                    return "Spades";
+                case 2:
+                    return "Hearts";
+                case 3:
+                    return "Clubs";
+            }
+            return "Diamonds";
+        }
+
+        private static string RandomValue()
+        {
+            int v = random.Next(1, 14);
+
+            switch (v)
+            {
+                case 1:
+                    return "Ace";
+                case 11:
+                    return "Jack";
+                case 12:
+                    return "Queen";
+                case 13:
+                    return "King";
+            }
+            return v.ToString();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace PickACardUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] pickedCards = CardPicker.PickSomeCards((int)numberOfCards.Value);
+            listOfCards.Items.Clear();
+            foreach (string card in pickedCards)
+            {
+                listOfCards.Items.Add(card);
+            }
+        }
+    }
+}
